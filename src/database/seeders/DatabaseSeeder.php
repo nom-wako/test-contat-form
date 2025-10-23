f496deea116b3590769be49fb6acebb8b5bf51ea
wako nomura <nom.wako369@gmail.com>
2025-10-23 20:56:51 +0900

    お問い合わせ用シーダーファイル作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Contact;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             CategorySeeder::class,
         ]);
+        Contact::factory(30)->create();
     }
 }
